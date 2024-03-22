83e1ebaafe641707f0cf49e2ea33eb1e436f2cbb
dcscitg <163496513+dcscitg@users.noreply.github.com>
2024-03-22 09:36:48 +0000

    Update AppTest.java

--- src/test/java/com/print/app/AppTest.java
+++ src/test/java/com/print/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello Worldssddsf!", app.getMessage());
+        assertEquals("Hello Worlds!", app.getMessage());
     }
 }
