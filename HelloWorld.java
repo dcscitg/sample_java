028cae8710692e370f6029555e4db0593cf2c146
dcscitg <163496513+dcscitg@users.noreply.github.com>
2024-03-22 05:13:51 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello worldslkjdfosidf!");
-    System.out.println("Hello worldslkjdfosidf!");
+    System.out.println("Hello worlds!");
 }
 }
