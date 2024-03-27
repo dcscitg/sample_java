2c9753b2145ba364dde4e14f5b8e94be5f9fa812
dcscitg <163496513+dcscitg@users.noreply.github.com>
2024-03-27 11:16:36 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,19 +11,16 @@ set +x
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-# Sanitize the artifactId (NAME) by removing any invalid characters
-NAME=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout | tr -cd '[[:alnum:]]-')
+NAME=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-# Sanitize the version (VERSION) by removing any invalid characters
-VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tr -cd '[[:alnum:]]-')
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar "target/${NAME}-${VERSION}.jar"
-
